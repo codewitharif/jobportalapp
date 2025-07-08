46765c66f1841000e40bf7496542fad749978943
Md Arif <aatif149209@gmail.com>
2025-07-08 19:44:52 +0530

    final version test 2

--- client/pages/ManageJobs.jsx
+++ client/pages/ManageJobs.jsx
@@ -67,9 +67,9 @@ const ManageJobs = () => {
         <h2 className="text-2xl font-semibold">Manage Posted Jobs</h2>
         <button
           onClick={() => navigate("/dashboard/add-job")}
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition sm:px-2 py-2"
         >
-          + Add New Job
+          +
         </button>
       </div>
 
