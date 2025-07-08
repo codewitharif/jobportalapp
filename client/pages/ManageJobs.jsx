8afb6cfce757374bb99e2882f9be9a308a5fc528
Md Arif <aatif149209@gmail.com>
2025-07-08 19:48:46 +0530

    final version test 2

--- client/pages/ManageJobs.jsx
+++ client/pages/ManageJobs.jsx
@@ -64,7 +64,9 @@ const ManageJobs = () => {
   return (
     <div className="p-6">
       <div className="flex justify-between items-center mb-6">
-        <h2 className="text-2xl font-semibold">Manage Posted Jobs</h2>
+        <h2 className="text-2xl font-semibold  sm:text-balance ">
+          Manage Jobs
+        </h2>
         <button
           onClick={() => navigate("/dashboard/add-job")}
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition sm:px-2 py-2"
